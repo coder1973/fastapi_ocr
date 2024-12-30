5431ebf075d4286590bd03b484e3f142ef2d4fa3
coder1973 <bulentozturk2019@outlook.com>
2024-12-30 12:42:14 +0100

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from fastapi import Fast
+from fastapi import FastAPI, File, UploadFile
+import shutil
+import pytesseract
+
+app = FastAPI()
+
+@app.post('ocr')
+def ocr(image: UploadFile = File(...)):
+    filePath = 'txtFile'
+    with open(filePath, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    return pytesseract.image_to_string(filePath, lang='eng')
